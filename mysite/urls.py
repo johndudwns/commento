53906a3b800915be7344c5b5b33b2622e876fd27
johndudwns <92994411+johndudwns@users.noreply.github.com>
2023-08-06 21:41:47 +0900

    4주차 댓글,관리자기능 전체 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-#from mysite import views
+# from mysite import views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('', views.index),
     path('<int:content_id>/', views.detail, name='detail'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
